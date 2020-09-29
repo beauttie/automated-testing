285ed265e604ac7f5bb11a9f9de1a55bd988bd1c
beauttie <bak02013@pomona.edu>
2020-09-29 01:52:38 -0700

    Write tests for to_s and reader methods

--- test/card_test.rb
+++ test/card_test.rb
@@ -26,6 +26,10 @@ describe Card do
     it "to_s returns a readable String value logically for values 2-10" do
       # Test to ensure that to_s works for cards values 2-10
       # for example:  "2 of diamonds"
+      (2..10).each do |card_value|
+        card = Card.new(card_value, :diamonds)
+        expect(card.to_s).must_equal "#{card_value} of diamonds"
+      end
     end
 
     it "to_s returns a readable String value for Ace, Jack, Queen, King" do
@@ -40,18 +44,39 @@ describe Card do
       #  11: Jack
       #  12: Queen
       #  13: King
+      face_cards = {
+          1 => "Ace",
+          11 => "Jack",
+          12 => "Queen",
+          13 => "King"
+      }
+
+      face_cards.each do |k, v|
+        card = Card.new(k, :hearts)
+        expect(card.to_s).must_equal "#{v} of hearts"
+      end
     end
   end
 
   describe "Reader methods" do
+    let (:card_1) { Card.new(1, :spades) }
+
+    let (:card_2) { Card.new(13, :diamonds) }
+
+    let (:card_3) { Card.new(10, :clubs) }
 
     it "Can retrieve the value of the card using a `.value`." do
       # ensure that `.value works as expected`
+      expect(card_1.value).must_equal 1
+      expect(card_2.value).must_equal 13
+      expect(card_3.value).must_equal 10
     end
 
     it "Can retrieve the value of the card using a `.suit`." do
       # ensure that `.suit works as expected returning the symbol of the suit`
-
+      expect(card_1.suit).must_equal :spades
+      expect(card_2.suit).must_equal :diamonds
+      expect(card_3.suit).must_equal :clubs
     end
   end
 
