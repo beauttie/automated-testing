5ac80569de0a5ff2010a2f6defdc6a26ba811eca
beauttie <bak02013@pomona.edu>
2020-09-29 01:53:36 -0700

    Implement constructor, draw, shuffle, count methods

--- lib/deck.rb
+++ lib/deck.rb
@@ -4,16 +4,29 @@
 require_relative 'card'
 
 class Deck
+  attr_reader :cards
 
   def initialize
+    @cards = []
 
+    [:hearts, :spades, :clubs, :diamonds].each do |suit|
+      (1..13).each do |value|
+        @cards << Card.new(value, suit)
+      end
+    end
   end
 
   def draw
     # returns a card
+    @cards.length > 0 ? ( return @cards.pop ) : ( raise NoMethodError.new("You have drawn all cards from the deck.") )
   end
 
   def shuffle
     # shuffles the deck
+    @cards.shuffle
+  end
+
+  def count
+    return @cards.length
   end
 end
