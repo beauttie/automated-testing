b4f4724f339ea0211ed9d4a3236a5cf87af28c6d
beauttie <bak02013@pomona.edu>
2020-09-29 01:51:35 -0700

    Implement constructor, to_face and to_s methods

--- lib/card.rb
+++ lib/card.rb
@@ -5,14 +5,28 @@ class Card
   attr_reader :value, :suit
 
   def initialize(value, suit)
-    @value = value
-    @suit = suit
-    
+    (1..13).include?(value) ? ( @value = value ) : ( raise ArgumentError.new("Invalid card value.") )
 
+    [:hearts, :spades, :clubs, :diamonds].include?(suit) ? ( @suit = suit ) : ( raise ArgumentError.new("Invalid card suit.") )
+  end
+
+  def to_face(value)
+    case value
+    when 1
+      return "Ace"
+    when 11
+      return "Jack"
+    when 12
+      return "Queen"
+    when 13
+      return "King"
+    else
+      return value
+    end
   end
 
   def to_s
-    return "#{value} of #{suit.to_s}"
+    return "#{to_face(value)} of #{suit.to_s}"
   end
 
 end
